5ceaffefbcf0fc3d75f28559146b25fe7e143b2a
Noor Muhammad <connectwithnoor1@gmail.com>
2023-02-19 19:03:52 +0500

    section footer newsletter

--- app/{section}/Footer/Footer.tsx
+++ app/{section}/Footer/Footer.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
+import { FooterOverlay, Newsletter } from '@/app/{components}';
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
-  </div>
+  <section className='app__footer section__padding' id='footer'>
+    <FooterOverlay />
+    <Newsletter />
+  </section>
 );
 
 export default Footer;
