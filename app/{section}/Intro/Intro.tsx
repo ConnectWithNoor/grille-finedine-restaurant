379a3c7fa4f012ab1e0e1d40dae129af23422d1a
Noor Muhammad <connectwithnoor1@gmail.com>
2023-02-19 15:03:30 +0500

    section video

--- app/{section}/Intro/Intro.tsx
+++ app/{section}/Intro/Intro.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+'use client';
 
+import { useRef, useState } from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const handleVideoClick = () => {
+    setIsPlaying((prev) => !prev);
+
+    !isPlaying ? videoRef.current?.play() : videoRef.current?.pause();
+  };
+
+  return (
+    <section className='app__video' id='intro'>
+      <video
+        src='/meal.mp4'
+        typeof='video/mp4'
+        loop
+        controls={false}
+        muted
+        ref={videoRef}
+      />
+
+      <div className='app__video-overlay flex__center'>
+        <div
+          className='app__video-overlay_circle flex__center'
+          onClick={handleVideoClick}
+        >
+          {isPlaying ? (
+            <BsPauseFill color='#fff' fontSize={30} />
+          ) : (
+            <BsFillPlayFill color='#fff' fontSize={30} />
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Intro;
